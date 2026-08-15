675b5124c312393e668983a33ce7107357cee914
Nishant Kumbhar <nishantsk210@gmail.com>
2026-08-15 09:32:16 +0530

    Created Controller

--- UserManagement.API/Controllers/UserController.cs
+++ UserManagement.API/Controllers/UserController.cs
@@ -1,12 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure.Core;
+using Microsoft.AspNetCore.Mvc;
 using UserManagement.API.DTOs;
 using UserManagement.API.Services;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace UserManagement.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class UserController:ControllerBase
+    public class UserController : ControllerBase
     {
         private readonly UserService _userService;
 
@@ -23,5 +25,69 @@ namespace UserManagement.API.Controllers
         }
 
 
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<UserResponse>> GetById(int id)
+        {
+            var user = await _userService.GetByIdAsync(id);
+            if (user is null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<UserResponse>> Create(CreateUserRequest request)
+        {
+            try
+            {
+                var user = await _userService.CreateAsync(request);
+
+                return CreatedAtAction(nameof(GetById), new { id = user.Id }, user);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return Conflict(new
+                {
+                    message = ex.Message,
+                });
+            }
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Update(int id, UpdateUserRequest request)
+        {
+            try
+            {
+                var updated = await _userService.UpdateAsync(id, request);
+                if (!updated)
+                {
+                    return NotFound();
+                }
+
+
+                return NoContent();
+            }
+            catch (InvalidOperationException ex)
+            {
+                return Conflict(new
+                {
+                    message = ex.Message,
+                });
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var deleted = await _userService.DeleteAsync(id);
+            if (!deleted)
+            {
+                return NotFound();
+            }
+            return NoContent();
+        }
     }
 }
+
+//BlueSky@123
