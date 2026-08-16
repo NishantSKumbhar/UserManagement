17e8af9e95ce58913724dc98f292b9bbe5138e76
Nishant Kumbhar <nishantsk210@gmail.com>
2026-08-16 20:41:49 +0530

    Working on JWT and Authentication

---
+++ UserManagement.API/Authentication/JwtService.cs
@@ -0,0 +1,19 @@
+﻿using UserManagement.API.Models;
+
+namespace UserManagement.API.Authentication
+{
+    public class JwtService
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateToken(User user, IEnumerable<string> roles)
+        {
+
+        }
+    }
+}
