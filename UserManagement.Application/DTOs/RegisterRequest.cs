3923b1be8ec821953fe075f879a758766986b7bc
Nishant Kumbhar <nishantsk210@gmail.com>
2026-08-20 22:36:21 +0530

    Created clean architecture design with separate class libraries

---
+++ UserManagement.Application/DTOs/RegisterRequest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagement.Application.DTOs
+{
+    public class RegisterRequest
+    {
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(255)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(8)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
